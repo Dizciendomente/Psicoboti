1a8c0b4f729f2076bb2deaa8e53399869b047a94
Dizciendomente <lic.florenciadiz@gmail.com>
2026-05-06 01:30:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,5 @@ app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
