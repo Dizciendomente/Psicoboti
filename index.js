1bf3bce6814f41d5cf9f3578975ee425469efe7f
Dizciendomente <lic.florenciadiz@gmail.com>
2026-05-08 20:59:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ Puedo ayudarte con información o con la reserva de tu primera sesión.`
       action: {
         buttons: [
           { type: "reply", reply: { id: "turno", title: "Reservar turno" } },
-          { type: "reply", reply: { id: "honorarios", title: "Honorarios / valor sesión" } },
+          { type: "reply", reply: { id: "honorarios", title: "Valor de la Sesión" } },
           { type: "reply", reply: { id: "modalidad", title: "Modalidades disponibles" } }
         ]
       }
